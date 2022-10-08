fc8e9b15c4dd1db01d0d3ce540389b01b58e8997
k-pantelidis <55916853+k-pantelidis@users.noreply.github.com>
2022-10-08 11:56:19 +0300

    Update checkMissing.py

--- checkMissing.py
+++ checkMissing.py
@@ -4,6 +4,7 @@ import os
 df = pd.read_csv('filesList.csv')
 df2 = pd.DataFrame(columns=['filename'])
 count = 0
+
 for i in range(len(df)):
     file_name = df.at[i, 'url']
     if os.path.exists('data/' + file_name) == False:
@@ -11,3 +12,5 @@ for i in range(len(df)):
         print(file_name)
         count += 1
         print(count)
+
+df.to_csv('missing.csv', index=False)
